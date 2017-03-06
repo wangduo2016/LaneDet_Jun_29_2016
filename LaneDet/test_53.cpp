2300084695d961ed9852c9a61162cd1cb173c15a
wangduo <931700659@qq.com>
2017-03-06 16:31:00 +0800

    test_53 and test_54

--- LaneDet/test_53.cpp
+++ LaneDet/test_53.cpp
@@ -1,84 +1,88 @@
-//#include <iostream>
-//#include <vector>
-////#include <bitset>
-////#include <list>
-////#include <deque>
-//#include <string>
-////#include <fstream>
-////#include <sstream>
-////#include <algorithm>
-////#include <numeric>
-////#include <iterator>
-////#include <stack> // stack adaptor
-////#include <queue> // both queue and priority_queue adaptors
-////#include <map>
-////#include <set>
-//#include "Sales_item.h"
-////#include "Sales_item1.h"
-////#include "Person.h"
-////#include "Screen.h"
-////#include "TextQuery.h"
-////#include "NoName.h"
-//using namespace std;
-//
-///////////////////////////////////2016-12-18 20:41:30
-//int main()
-//{
-//	// 第十四章 重载操作符与转换
-//	int v1, v2;
-//	cout << "The sum of " << v1 << " and " << v2
-//		<< " is " << v1 + v2 << endl;
-//
-//	// 14.1. 重载操作符的定义
-//
-//	// 重载的操作名
-//
-//	// 重载操作符必须具有一个类类型操作数
-//	// error: cannot redefine built-in operator for ints
-//	//int operator+(int, int);
-//
-//	// 优先级和结合性是固定的
-//	//x == y + z;
-//
-//	// 不再具备短路求值特性
-//
-//	// 类成员与非成员
-//
-//	// 操作符重载和友元关系
-//
-//	// 使用重载操作符
-//	Sales_item item1, item2;
-//	cout << item1 + item2 << endl;
-//
-//	// equivalent direct call to nonmember operator function
-//	cout << operator+(item1, item2) << endl;
-//
-//	item1 += item2; // expression based "call"
-//	item1.operator+=(item2); // equivalent call to member operator function
-//
-//	// 14.1.1. 重载操作符的设计
-//
-//	// 不要重载具有内置含义的操作符
-//
-//	// 大多数操作符对类对象没有意义
-//
-//	// 复合赋值操作符
-//
-//	// 相等和关系操作符
-//
-//	// 选择成员或非成员实现
-//
-//	// 14.2. 输入和输出操作符
-//
-//	//vector<int> iv{ 5, 4, 3, 2, 1 };
-//	//int a = 2, b = 1;
-//
-//	// c++11
-//	//[b](int &x) {cout << (x + b) << endl; }
-//	//[=](int &y) {y *= (a + b); }
-//	//[=](int &z)->int {return z * (a + b); }
-//
-//
-//
-//	return 0;
-//}
+#include <iostream>
+#include <vector>
+//#include <bitset>
+//#include <list>
+//#include <deque>
+#include <string>
+//#include <fstream>
+//#include <sstream>
+//#include <algorithm>
+//#include <numeric>
+//#include <iterator>
+//#include <stack> // stack adaptor
+//#include <queue> // both queue and priority_queue adaptors
+//#include <map>
+//#include <set>
+#include "Sales_item.h"
+//#include "Sales_item1.h"
+//#include "Person.h"
+//#include "Screen.h"
+//#include "TextQuery.h"
+//#include "NoName.h"
+#include "ScreenPtr.h"
+using namespace std;
+
+/////////////////////////////////2016-12-18 20:41:30
+int main()
+{
+	// 第十四章 重载操作符与转换
+	int v1, v2;
+	cout << "The sum of " << v1 << " and " << v2
+		<< " is " << v1 + v2 << endl;
+
+	// 14.1. 重载操作符的定义
+
+	// 重载的操作名
+
+	// 重载操作符必须具有一个类类型操作数
+	// error: cannot redefine built-in operator for ints
+	//int operator+(int, int);
+
+	// 优先级和结合性是固定的
+	//x == y + z;
+
+	// 不再具备短路求值特性
+
+	// 类成员与非成员
+
+	// 操作符重载和友元关系
+
+	// 使用重载操作符
+	Sales_item item1, item2;
+	cout << item1 + item2 << endl;
+
+	// equivalent direct call to nonmember operator function
+	cout << operator+(item1, item2) << endl;
+
+	item1 += item2; // expression based "call"
+	item1.operator+=(item2); // equivalent call to member operator function
+
+	// 14.1.1. 重载操作符的设计
+
+	// 不要重载具有内置含义的操作符
+
+	// 大多数操作符对类对象没有意义
+
+	// 复合赋值操作符
+
+	// 相等和关系操作符
+
+	// 选择成员或非成员实现
+
+	// 14.2. 输入和输出操作符
+
+	//vector<int> iv{ 5, 4, 3, 2, 1 };
+	//int a = 2, b = 1;
+
+	// c++11
+	//[b](int &x) {cout << (x + b) << endl; }
+	//[=](int &y) {y *= (a + b); }
+	//[=](int &z)->int {return z * (a + b); }
+
+	// 14.6. 成员访问操作符
+	// 构建更安全的指针
+	//ScreenPtr p1; // error: ScreenPtr has no default constructor
+	ScreenPtr ps(new Screen(4, 4)); // ok: ps points to a copy of myScreen
+
+	return 0;
+}
--- LaneDet/test_54.cpp
+++ LaneDet/test_54.cpp
@@ -1,50 +1,50 @@
-#include <iostream>
-#include <string>
-#include "Item_base.h"
-using namespace std;
-
-////////////////////////////2017-1-9 21:10:34
-int main()
-{
-	// 15.2.4. virtual 与其他成员函数
-	//// 从派生类型到基类的转换
-	//// function with an Item_base reference parameter
-	//double print_total1(const Item_base&, size_t);
-
-	//Item_base item; // object of base type
-	//// ok: use pointer or reference to Item_base to refer to an Item_base object
-	//print_total1(item, 10); // passes reference to an Item_base object
-	//Item_base *p = &item; // p points to an Item_base object
-	//Bulk_item bulk; // object of derived type
-	//// ok: can bind a pointer or reference to Item_base to a Bulk_item object
-	//print_total1(bulk, 10); // passes reference to the Item_base part of bulk
-	//p = &bulk; // p points to the Item_base part of bulk
-
-	// 可以在运行时确定 virtual 函数的调用
-	Item_base base;
-	Bulk_item derived;
-	// print_total makes a virtual call to net_price
-	print_total(cout, base, 10); // calls Item_base::net_price
-	print_total(cout, derived, 10); // calls Bulk_item::net_price
-
-	///////////////////////////////////2017-1-10
-	// 15.2.5. 公用、私有和受保护的继承
-	// 派生访问标号将控制派生类的用户对从 Base 继承而来的成员的访问：
-	Base b;
-	Public_derived d1;
-	Private_derived d2;
-	b.basemem(); // ok: basemem is public
-	d1.basemem(); // ok: basemem is public in the derived class
-	//d2.basemem(); // error: basemem is private in the derived class
-
-	// Exercise 15.13:
-	C2 c2;
-	C2 *pc2 = &c2;
-	// C2 类型的对象访问static成员的四种方式
-	ConcreteBase::object_count();
-	C2::object_count();
-	c2.object_count();
-	pc2->object_count();
-
-	return 0;
-}
+//#include <iostream>
+//#include <string>
+//#include "Item_base.h"
+//using namespace std;
+//
+//////////////////////////////2017-1-9 21:10:34
+//int main()
+//{
+//	// 15.2.4. virtual 与其他成员函数
+//	//// 从派生类型到基类的转换
+//	//// function with an Item_base reference parameter
+//	//double print_total1(const Item_base&, size_t);
+//
+//	//Item_base item; // object of base type
+//	//// ok: use pointer or reference to Item_base to refer to an Item_base object
+//	//print_total1(item, 10); // passes reference to an Item_base object
+//	//Item_base *p = &item; // p points to an Item_base object
+//	//Bulk_item bulk; // object of derived type
+//	//// ok: can bind a pointer or reference to Item_base to a Bulk_item object
+//	//print_total1(bulk, 10); // passes reference to the Item_base part of bulk
+//	//p = &bulk; // p points to the Item_base part of bulk
+//
+//	// 可以在运行时确定 virtual 函数的调用
+//	Item_base base;
+//	Bulk_item derived;
+//	// print_total makes a virtual call to net_price
+//	print_total(cout, base, 10); // calls Item_base::net_price
+//	print_total(cout, derived, 10); // calls Bulk_item::net_price
+//
+//	///////////////////////////////////2017-1-10
+//	// 15.2.5. 公用、私有和受保护的继承
+//	// 派生访问标号将控制派生类的用户对从 Base 继承而来的成员的访问：
+//	Base b;
+//	Public_derived d1;
+//	Private_derived d2;
+//	b.basemem(); // ok: basemem is public
+//	d1.basemem(); // ok: basemem is public in the derived class
+//	//d2.basemem(); // error: basemem is private in the derived class
+//
+//	// Exercise 15.13:
+//	C2 c2;
+//	C2 *pc2 = &c2;
+//	// C2 类型的对象访问static成员的四种方式
+//	ConcreteBase::object_count();
+//	C2::object_count();
+//	c2.object_count();
+//	pc2->object_count();
+//
+//	return 0;
+//}
